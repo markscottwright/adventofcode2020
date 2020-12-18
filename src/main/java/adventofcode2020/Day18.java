80524407352059f61c38e829844e40fd444bce27
Mark Wright <Mark.Wright@entrust.com>
2020-12-18 16:32:50 -0500

    Day 18

--- src/main/java/adventofcode2020/Day18.java
+++ src/main/java/adventofcode2020/Day18.java
@@ -4,6 +4,14 @@ import java.util.ArrayList;
 import java.util.regex.Pattern;
 
 public class Day18 {
+    public static <T> T pop(ArrayList<T> stack) {
+        return stack.remove(stack.size() - 1);
+    }
+
+    public static <T> T last(ArrayList<T> stack) {
+        return stack.get(stack.size() - 1);
+    }
+
     static class Expression {
         private ArrayList<String> items;
         private ArrayList<String> originalItems;
@@ -16,16 +24,25 @@ public class Day18 {
 
         @Override
         public String toString() {
-            return "Expression: " + items + "\n    :" + originalItems;
+            return "Expression: " + items + " (" + originalItems + ")";
         }
 
-        static Expression parse(String l) {
+        static Expression parsePart1(String l) {
             ArrayList<String> items = new ArrayList<>();
             var pattern = Pattern.compile("[0-9]+|[()+*]");
             var matcher = pattern.matcher(l);
             while (matcher.find())
                 items.add(matcher.group());
-            return new Expression(toRpn(items), items);
+            return new Expression(toRpnPart1(items), items);
+        }
+
+        static Expression parsePart2(String l) {
+            ArrayList<String> items = new ArrayList<>();
+            var pattern = Pattern.compile("[0-9]+|[()+*]");
+            var matcher = pattern.matcher(l);
+            while (matcher.find())
+                items.add(matcher.group());
+            return new Expression(toRpnPart2(items), items);
         }
 
         long eval() {
@@ -43,31 +60,74 @@ public class Day18 {
             return stack.get(0);
         }
 
-        private long pop(ArrayList<Long> stack) {
-            return stack.remove(stack.size() - 1);
+        /**
+         * Should have been using this all along
+         * https://en.wikipedia.org/wiki/Shunting-yard_algorithm
+         * 
+         * @param items
+         *            expression items in infix order
+         * @return items in RPN order with part2 precedence (aka * binds looser than
+         *         +)
+         */
+        static ArrayList<String> toRpnPart2(ArrayList<String> items) {
+            var out = new ArrayList<String>();
+            var operatorStack = new ArrayList<String>();
+
+            for (String item : items) {
+                if (isNumber(item)) {
+                    out.add(item);
+                } else if (item.equals("("))
+                    operatorStack.add(item);
+                else if (item.equals(")")) {
+                    String operator;
+                    while (!(operator = pop(operatorStack)).equals("("))
+                        out.add(operator);
+                } else if (!operatorStack.isEmpty()
+                        && precedence(item) <= precedence(last(operatorStack))
+                        && !last(operatorStack).equals("(")) {
+                    String previousStackTop = pop(operatorStack);
+                    out.add(previousStackTop);
+                    operatorStack.add(item);
+                } else {
+                    operatorStack.add(item);
+                }
+            }
+
+            while (operatorStack.size() > 0)
+                out.add(pop(operatorStack));
+            return out;
         }
 
-        static ArrayList<String> toRpn(ArrayList<String> items) {
+        private static int precedence(String item) {
+            if (item.equals("("))
+                return 3;
+            else if (item.equals("+"))
+                return 2;
+            assert item.equals("*"); // we only have two operators
+            return 1;
+        }
+
+        static ArrayList<String> toRpnPart1(ArrayList<String> items) {
             ArrayList<String> rpn = new ArrayList<>();
-            toRpn(rpn, items, 0);
+            toRpnPart1(rpn, items, 0);
             return rpn;
         }
 
-        static int pushNext(ArrayList<String> rpn, ArrayList<String> items,
+        static int pushNextPart1(ArrayList<String> rpn, ArrayList<String> items,
                 int pos) {
             String item = items.get(pos);
             if (isNumber(item)) {
                 rpn.add(item);
                 return pos + 1;
             } else if (item.equals("(")) {
-                return toRpn(rpn, items, pos + 1);
+                return toRpnPart1(rpn, items, pos + 1);
             } else {
                 throw new RuntimeException(
                         "Unexpectd item " + item + " at:" + pos);
             }
         }
 
-        static int toRpn(ArrayList<String> rpn, ArrayList<String> items,
+        static int toRpnPart1(ArrayList<String> rpn, ArrayList<String> items,
                 int i) {
             while (i < items.size()) {
                 String item = items.get(i);
@@ -77,9 +137,9 @@ public class Day18 {
                 } else if (item.equals(")"))
                     return i + 1;
                 else if (item.equals("(")) {
-                    i = toRpn(rpn, items, i + 1);
+                    i = toRpnPart1(rpn, items, i + 1);
                 } else if (item.equals("+") || item.equals("*")) {
-                    i = pushNext(rpn, items, i + 1);
+                    i = pushNextPart1(rpn, items, i + 1);
                     rpn.add(item);
                 }
             }
@@ -94,11 +154,14 @@ public class Day18 {
     public static void main(String[] strings) {
         long part1Sum = 0;
         for (Expression expression : Util.parseAndCollectForDay(18,
-                Expression::parse)) {
+                Expression::parsePart1)) {
             part1Sum += expression.eval();
         }
-        ;
         System.out.println("Day 18 part 1: " + part1Sum);
+
+        long part2Sum = Util.parseAndCollectForDay(18, Expression::parsePart2)
+                .stream().mapToLong(Expression::eval).sum();
+        System.out.println("Day 18 part 2: " + part2Sum);
     }
 
 }
--- src/test/java/adventofcode2020/Day18Test.java
+++ src/test/java/adventofcode2020/Day18Test.java
@@ -1,20 +1,34 @@
 package adventofcode2020;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.*;
 
 import org.junit.Test;
 
 public class Day18Test {
 
     @Test
-    public void test() {
-        assertThat(Day18.Expression.parse("2 * 3 + (4 * 5").eval())
+    public void testPart1() {
+        assertThat(Day18.Expression.parsePart1("2 * 3 + (4 * 5").eval())
                 .isEqualTo(26);
-        assertThat(Day18.Expression.parse("5 + (8 * 3 + 9 + 3 * 4 * 3)").eval())
-                .isEqualTo(437);
+        assertThat(Day18.Expression.parsePart1("5 + (8 * 3 + 9 + 3 * 4 * 3)")
+                .eval()).isEqualTo(437);
         assertThat(Day18.Expression
-                .parse("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2")
+                .parsePart1("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2")
                 .eval()).isEqualTo(13632);
+    }
+
+    @Test
+    public void testPart2() throws Exception {
+        assertThat(Day18.Expression.parsePart2("1 + (2 * 3) + (4 * (5 + 6)) ")
+                .eval()).isEqualTo(51);
+        assertThat(Day18.Expression.parsePart2("2 * 3 + (4 * 5)").eval())
+                .isEqualTo(46);
+        assertThat(Day18.Expression.parsePart2("5 + (8 * 3 + 9 + 3 * 4 * 3) ")
+                .eval()).isEqualTo(1445);
+        assertThat(Day18.Expression
+                .parsePart2("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))").eval())
+                        .isEqualTo(669060);
     }
 
 }
